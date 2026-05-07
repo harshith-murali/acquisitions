2de5826c3957b6d8bd71f5273ba105105a07be60
Harshith M <mharshith200@gmail.com>
2026-05-07 22:40:07 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello from Acquisitions Service");
+});
+
+export default app;
